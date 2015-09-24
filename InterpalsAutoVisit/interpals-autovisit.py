07709c2c3c4c3c3ecd6f38ccc0019aad9f8ced62
web-ap <contact@web-ap.fr>
2015-09-24 15:37:53 +0200

    Update with a newest Python

--- InterpalsAutoVisit/interpals-autovisit.py
+++ InterpalsAutoVisit/interpals-autovisit.py
@@ -18,7 +18,7 @@ sex = '' # Possible values: 'male', 'female', or empty '' for both
 cont = '' # Possible values: 'AF' (Africa) 'AS' (Asia), 'EU' (Europe)
             #'NA' (North America), 'OC' (Oceania), 'SA' (South America)
 age1 = 18 # minimum age
-age2 = 38 # maximum age
+age2 = 30 # maximum age
 
 # We keep already visited profile in a file to avoid visiting
 # a profile multiple times on consecutive execution of this script
@@ -37,39 +37,39 @@ sessioncount = 0
 run_number = 1
 f = open(visitedUsersFilename, 'a+')
 processedUsers = [line.strip() for line in f]
-print "Already processed " + str(len(processedUsers)) + " users."
+print("Already processed ", str(len(processedUsers)), " users.")
 s = requests.Session()
-login = raw_input('Username (mail): ')
+login = input('Username (mail): ')
 password = getpass.getpass('Password: ')
 payload = {'action': 'login',
            'login': login,
            'auto_login' : '0',
            'password': password}
 
-print "Logging in..."
+print("Logging in...")
 s.get("http://www.interpals.net/")
 r = s.post("http://www.interpals.net/login.php", data=payload)
-print "Logged in: Starting the dance \o/"
+print("Logged in: Starting the dance \o/")
 time.sleep(2)
 #print r.headers
 #print r.text
 while True:
     runcount = 0
     if DEBUG:
-        print "Fetching online users page..."
+        print("Fetching online users page...")
     r = s.get("http://www.interpals.net/online.php?sex=%s&cont=%s&age1=%d&age2=%d" % (sex, cont, age1, age2))
     data = r.text
     usernames = re.findall(r'<div class=\'online_prof\'><a href=\'([a-zA-Z0-9\-_]+)\'', data, re.M)
     for username in usernames:
         if username not in processedUsers:
             if DEBUG:
-                print "Visiting profile of " + username + " (" + str(sessioncount) + ")"
+                print("Visiting profile of " + username + " (" + str(sessioncount) + ")")
             runcount += 1
             sessioncount += 1
             r = s.get("http://www.interpals.net/" + username)
             waitTime = random.randrange(MIN_DELAY_BETWEEN_PROFILE_VIEWS*10, MAX_DELAY_BETWEEN_PROFILE_VIEWS*10) / float(10)
             if DEBUG:
-                print "Waiting " + str(waitTime) + "s"
+                print("Waiting " , str(waitTime) , "s")
             else:
                 os.system('cls' if os.name=='nt' else 'clear')
                 print ('\rRun %d - Fetched %d users (%d total)' % (run_number, runcount, sessioncount))
@@ -77,7 +77,7 @@ while True:
             processedUsers.append(username)
             f.write(username + "\n")
         elif DEBUG:
-            print "Already visited " + username
+            print("Already visited " , username)
     run_number += 1
     if (runcount < 20):
         print ('[-] Waiting (%d)s before next run (fetched %d users this run, %d total)\n' % (DELAY_BETWEEN_RUNS * 4, runcount, sessioncount))
